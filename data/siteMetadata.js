ac8d4c74a9fda340e96cbca26fd0c9034b9232b9
Thanesh Pannirselvam <thaneshp@Thaneshs-MacBook-Air.local>
2025-02-06 21:51:45 +1100

    feat: updates metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Thanesh P.',
+  author: 'Thanesh P.',
+  headerTitle: 'Thanesh P.',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
